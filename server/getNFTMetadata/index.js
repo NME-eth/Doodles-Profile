cc45557d1a6f3245d5dd70e290ade00cf6c85313
NME.eth <maximus.holm@gmail.com>
2022-03-26 23:54:08 +0200

    created endpoint to get the NFT's metadata using alchemy

--- server/getNFTMetadata/index.js
+++ server/getNFTMetadata/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
-//const fetch = require('node-fetch');
+
+const alchemy = require('@alch/alchemy-web3');
+const web3 = alchemy.createAlchemyWeb3(
+  `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}` 
+);
+
+router.get('/:contractAddress/:tokenId', async (req, res) => {
+  const contractAddress = req.params.contractAddress;
+  const tokenId = req.params.tokenId;
+
+  console.log('fetching nft data');
+  console.log(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);
+  const response = await web3.alchemy.getNftMetadata({
+    contractAddress,
+    tokenId,
+  });
+  console.log(response);
+  res.json(response);
+})
 
 router.get('/', (req, res) => {
-  res.json({ success: "Hello NFT MetaDATA" });
+  res.json({ message: "Hello from Meta data endpoint" });
 })
 
 module.exports = router;
