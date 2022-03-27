db04915e8766969b913983f0b17fd1e13f161df2
NME.eth <maximus.holm@gmail.com>
2022-03-27 21:58:02 +0300

    end point now returns nft metadata

--- server/getNFTMetadata/index.js
+++ server/getNFTMetadata/index.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const router = express.Router();
 
@@ -6,22 +7,28 @@ const web3 = alchemy.createAlchemyWeb3(
   `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}` 
 );
 
-router.get('/:contractAddress/:tokenId', async (req, res) => {
-  const contractAddress = req.params.contractAddress;
-  const tokenId = req.params.tokenId;
+router.get('/', async (req, res) => {
+  const contractAddress = req.query.contractAddress;
+  const rawId = req.query.tokenId;
+  const idList = typeof rawId === 'string' ? [rawId] : rawId;
 
-  console.log('fetching nft data');
-  console.log(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);
-  const response = await web3.alchemy.getNftMetadata({
-    contractAddress,
-    tokenId,
-  });
-  console.log(response);
-  res.json(response);
-})
+  let metadataList = [];
+  // using normal loop incase we get rate limited
+  for (let i = 0; i < idList.length; i++) {
+    const response = await web3.alchemy.getNftMetadata({
+      contractAddress,
+      tokenId: idList[i],
+      tokenType: 'ERC721'
+    });
 
-router.get('/', (req, res) => {
-  res.json({ message: "Hello from Meta data endpoint" });
+    metadataList[i] = {
+      name: response.metadata.name,
+      id: idList[i],
+      image: response.tokenUri.gateway,
+      attributes: response.metadata.attributes,
+    };
+  }
+  res.json(metadataList);
 })
 
 module.exports = router;
