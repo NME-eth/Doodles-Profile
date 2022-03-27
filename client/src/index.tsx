3433a10c6c524f5d679cbfc69016e656c573c6ea
NME.eth <maximus.holm@gmail.com>
2022-03-28 00:30:50 +0300

    removing usedapp wrapper because package no longer used

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DAppProvider } from '@usedapp/core';
 import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DAppProvider config={{}}>
       <App />
-    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
